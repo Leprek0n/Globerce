f3eb5076adccf356861a85cafaa0053f5393bca0
Daulet <84085865+Leprek0n@users.noreply.github.com>
2022-09-22 22:17:58 +0600

    Update c.py
    init

--- c.py
+++ c.py
@@ -8,7 +8,7 @@ server = '127.0.0.1', 80  # Данные сервера
  
 a = ''
 b = 0
-alias = input() # Вводим наш псевдоним
+alias = input("Enter your username:") # Вводим наш псевдоним
 sor = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sor.bind(('', 0)) # Задаем сокет как клиент
 sor.sendto((alias+' Connect to server').encode('utf-8'), server)# Уведомляем сервер о подключении
@@ -16,7 +16,7 @@ potok = threading.Thread(target= read_sok)
 potok.start()
 while 1 :
     try:
-        mensahe = input()
+        mensahe = input("Message:")
         if mensahe == '0-1':
             sor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             break
